94a65ddda858ef778e00ff38577b80b8c3ab8e32
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-10-04 20:52:04 +1030

    display for current date and time in jumbotron

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// set the current date and time in jumbtron
+var currentDay = moment().format("dddd, MMMM Do");
+
+// display current date and time in jumbotron
+$("#currentDay").text(currentDay);
+
+
