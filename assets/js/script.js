0fefb5b75730ec30bd51123fcfd5e92077190004
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-10-05 16:30:08 +1030

    time-block colors js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,35 @@
+// HEADER
 // set the current date and time in the jumbotron
 var currentDay = moment().format("dddd, MMMM Do");
 
 // display current date and time in the jumbotron
 $("#currentDay").text(currentDay);
 
+
+// MAIN SCHEDULER
+
+// function to check what time-block we are in:
+// past, present, future
+function updateTime() {
+    var currentTime = moment().hours();
+
+$('.time-block').each(function () {
+    var hourTime = parseInt($(this).attr('id').split('-'[1]));
+
+    if(hourTime < currentTime) { // if timeblock is not the current time, then the color will show as past.
+        $(this).addClass('past');
+    } else if (hourTime === currentTime) { // if timeblock is current time, then the color will show as present.
+        $(this).removeClass('past');
+        $(this).addClass('present');
+    } else {
+        $(this).removeClass('past'); // if timeblock has not happened yet, then the color will show as future.
+        $(this).removeClass('present');
+        $(this).addClass('future');
+    }
+});
+}
+
+updateTime();
+
+ 
 
